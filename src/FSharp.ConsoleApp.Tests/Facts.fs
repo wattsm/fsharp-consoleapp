e66cbfe17256f6d9767155e9d2cea98f1ff40a99
Mark Watts <mark.watts@hotmail.co.uk>
2014-05-19 08:14:23 +0100

    Add source code

---
+++ src/FSharp.ConsoleApp.Tests/Facts.fs
@@ -0,0 +1,155 @@
+﻿namespace FSharp.ConsoleApp.Tests
+
+open System
+open Xunit
+open FSharp.ConsoleApp
+
+module ``Args facts`` = 
+
+    [<Trait ("Module", "Args")>]
+    module ``parse facts`` = 
+        
+        let [<Fact>] ``Arguments prefixed with a double dash are flags`` () = 
+
+            let values = [| "--switch" |]
+            let args = Args.parse values
+
+            let switch =
+                match args with
+                | [ (Flag flag) ] -> flag
+                | _ -> String.Empty
+
+            Assert.Equal<String> ("switch", switch)
+
+        let [<Fact>] ``Arguments prefixed with a dash and followed by an argument with no prefix are settings`` () = 
+        
+            let values = [| "-key"; "value" |]
+            let args = Args.parse values
+
+            let key, value = 
+                match args with
+                | [ (Setting (key, value)) ] -> key, value
+                | _ -> String.Empty, String.Empty
+
+            Assert.Equal<String> ("key", key)
+            Assert.Equal<String> ("value", value)
+
+        let [<Fact>] ``Arguments prefixed with a dash but not followed by an argument with no prefix are ignored`` () = 
+
+            let values = [| "-key" |] 
+            let args = Args.parse values
+
+            Assert.True (List.isEmpty args)
+
+        let [<Fact>] ``Argument without prefix at the head of the input array are commands`` () =
+
+            let values = [| "cmd"; |]
+            let args = Args.parse values
+
+            let command = 
+                match args with
+                | [ (Command command) ] -> command
+                | _ -> String.Empty
+
+            Assert.Equal<String> ("cmd", command)
+
+        let [<Fact>] ``Arguments without prefix not at the head of the input array or after setting keys are ignored`` () = 
+
+            let values = [| "--switch"; "cmd" |]
+            let args = Args.parse values
+
+            let isCorrect = 
+                match args with
+                | [ (Flag "switch") ] -> true
+                | _ -> false
+
+            Assert.True (isCorrect)
+
+module ``Dispatcher facts`` = 
+
+    [<Trait ("Module", "Dispatcher")>]
+    module ``exec facts`` = 
+
+        let [<Fact>] ``Correct handler is called when command is present in arguments`` () = 
+        
+            let args = [ (Command "x"); ]
+            let handlers = [ ("x", fun _ -> true); ("y", fun _ -> false); ]
+            let default' = fun _ -> false
+
+            let result = Dispatcher.exec default' handlers args
+
+            Assert.True (result)
+
+        let [<Fact>] ``Command handler is called with remaining arguments`` () = 
+        
+            let args = [ (Command "x"); (Flag "switch"); ]
+            let default' = fun _ -> false
+
+            let handlers = [
+                ("x", fun args' -> args' = [ (Flag "switch") ]);
+                ("y", fun _ -> false);
+            ]
+
+            let result = Dispatcher.exec default' handlers args
+
+            Assert.True (result)
+
+        let [<Fact>] ``Default handler is called when no command is present in arguments`` () = 
+        
+            let args = [ (Flag "switch") ]
+            let default' = fun _ -> "Default"
+            let handlers = []
+
+            let result = Dispatcher.exec default' handlers args
+
+            Assert.Equal<String> ("Default", result)
+        
+        let [<Fact>] ``Default handler is called when no matching command is present in arguments`` () =
+        
+            let args = [ (Command "x"); (Flag "switch"); ]
+            let default' = fun _ -> true
+
+            let handlers = [
+                ("y", fun _ -> false);
+                ("z", fun _ -> false);
+            ]
+
+            let result = Dispatcher.exec default' handlers args
+
+            Assert.True (result)
+
+module ``App facts`` = 
+
+    [<Trait ("Module", "App")>]
+    module ``tryGetSetting facts`` = 
+
+        let [<Fact>] ``Some value returned when key is present`` () = 
+
+            let value = 
+                [ Setting ("key", "value") ]
+                |> App.tryGetSetting "key"
+                |> Option.get
+
+            Assert.Equal<String> ("value", value)
+
+        let [<Fact>] ``None returned when key is not present`` () =
+            
+            let isNone = 
+                [ Setting ("key", "value") ]
+                |> App.tryGetSetting "anotherKey"
+                |> Option.isNone
+
+            Assert.True (isNone)
+
+    [<Trait ("Module", "App")>]
+    module ``isFlagged facts`` = 
+
+        let [<Fact>] ``True when the flag is present`` () = 
+           [ (Flag "switch") ]
+           |> App.isFlagged "switch"
+           |> Assert.True
+
+        let [<Fact>] ``False when the flag is not present`` () = 
+            [ (Flag "switch") ]
+            |> App.isFlagged "anotherSwitch"
+            |> Assert.False
---
+++ src/FSharp.ConsoleApp/App.fs
@@ -0,0 +1,143 @@
+﻿namespace FSharp.ConsoleApp
+
+open System
+
+///Common utility functions
+[<AutoOpen>]
+module private Common =
+
+    ///True if two strings are the same, ignoring case
+    let areSame (x : String) (y : String) = 
+        String.Compare (x, y, true) = 0
+
+///Union describing argument types
+type Arg = 
+    | Command of String
+    | Flag of String
+    | Setting of (String * String)
+    
+///Functions for working with arguments
+[<RequireQualifiedAccess>]
+module Args =
+    
+    open System.Text.RegularExpressions
+
+    ///Union describing a command line token
+    type Token =     
+        | Key of (String * Boolean)
+        | Value of String
+
+    ///Functions for working with tokens
+    [<AutoOpen>]
+    module private Tokens =
+
+        ///An active pattern used to match regular expressions
+        let private (|RegEx|_|) pattern str = 
+            match (Regex.Match (str, pattern, RegexOptions.IgnoreCase)) with
+            | result when result.Success -> 
+                Array.ofSeq (seq {
+                    for i = 0 to (result.Groups.Count - 1) do
+                        yield result.Groups.[i].Value
+                }) |> Some
+            | _ -> None
+
+        ///Converts an array of command line arguments to an array of tokens
+        let tokenise = 
+            let tokeniseOne (str : String) = 
+                match str with
+                | RegEx "--(.+)" groups -> Key (groups.[1], true)
+                | RegEx "-(.+)" groups -> Key (groups.[1], false)
+                | _ -> Value str
+            in Array.Parallel.map tokeniseOne >> Array.toList
+
+    ///Parses an array of command line arguments to a list of app arguments
+    let parse =
+        
+        ///Reads the command from the head of the input array of tokens (if present)
+        let readCommand tokens = 
+            match tokens with
+            | Value command :: tokens' -> ([ Command command ], tokens')
+            | _ -> ([], tokens)
+
+        ///Reads the remaining (non-command) arguments from the input array of tokens
+        let rec readRemainder (args, tokens) = 
+            match tokens with
+            | Key (key, true) :: tokens' -> 
+
+                let args' = (Flag key) :: args
+                in readRemainder (args', tokens')
+
+            | Key (key, false) :: Value value :: tokens' -> 
+
+                let args' = Setting (key, value) :: args
+                in readRemainder (args', tokens')
+
+            | _ :: tokens' -> readRemainder (args, tokens')
+            | [] -> args
+
+        tokenise >>  readCommand >> readRemainder >> List.rev
+
+///Functions for invoking handlers
+[<RequireQualifiedAccess>]
+module Dispatcher =
+
+    ///Miscellaneous functions used by this module
+    [<AutoOpen>]
+    module private Misc =         
+    
+        ///Splits an array of app arguments into a command/arguments pair
+        let private asCommandPair = function
+            | Command command :: args -> (Some command, args)
+            | args -> (None, args)  
+
+        ///Attempts to get a value by key from a key/value list
+        let tryGetValue key = 
+            List.tryPick (function
+                | (key', value) when (areSame key key') -> Some value
+                | _ -> None
+            )
+
+        ///An active pattern used to match a command invocation from a list of app args
+        let (|Exec|_|) args = 
+            match (asCommandPair args) with
+            | (Some command, args') -> Some (command, args')
+            | _ -> None
+
+    ///Executes a handler based on the command line arguments, invoking a default if no matching
+    ///command is found.
+    let exec default' handlers = function
+        | Exec (command, args) ->
+            match (tryGetValue command handlers) with
+            | Some handler -> handler args
+            | _ -> default' ()            
+        | _ -> default' ()
+
+///Functions and types for running a console application
+[<RequireQualifiedAccess>]
+module App = 
+
+    ///Runs an application using the given handlers
+    let run default' handlers = Args.parse >> Dispatcher.exec default' handlers
+
+    ///Try and get a setting value by key from an argument list
+    let tryGetSetting key = 
+        List.tryPick (function
+            | Setting (key', value) when (areSame key key') -> Some value
+            | _ -> None
+        )
+
+    ///Get a setting value by key from an argument list
+    let getSetting key = tryGetSetting key >> Option.get
+
+    ///True if all keys provided have settings in an argument list
+    let hasSettings keys args = 
+        List.forall (fun key ->
+            Option.isSome (tryGetSetting key args)
+        ) keys
+
+    ///True if a flag is present in an argument list
+    let isFlagged flag = 
+        List.exists (function
+            | Flag flag' when (areSame flag flag') -> true
+            | _ -> false
+        )
